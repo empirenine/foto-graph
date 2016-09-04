8ad9fa17b6a827558ea2e97efc39d71cfe46f93f
Eric Schleicher <eric@brilligents.com>
2016-09-04 02:52:50 -0700

    Adding logic around the exif parsing. change from md5 library to md5-file

--- controllers/imageFile.js
+++ controllers/imageFile.js
@@ -1,18 +1,58 @@
 var Q = require('q');
 var fs = require('fs');
+var path = require('path');
 var b2 = require('backblaze-b2');
+var exif = require('exif-parser');
+var md5File = require('md5-file');
 var structr = require('structr-4-tasks');
 
-function _parseExif(file){
-
+function _parseExif(file, scanLength){
+    //the exif tag should be at the beginning of the file.
+    scanLength = scanLength || 16003;
+    return Q.promise(function(resolvePromise, rejectPromise){
+        try{
+            exif.parseTags(fs.read(path.resolve(file),null,0,scanLength))
+        }
+        catch(e){
+            rejectPromise(e);
+        }
+    });
 }
 
 function _hashFile(file){
-
+    return Q.promise(function(resolvePromise, rejectPromise){
+        try{
+            return md5File(path.resolve(file), function(err, data){
+                if(err){
+                    rejectPromise(err);
+                }
+                else{
+                    resolvePromise(data);
+                }
+            });
+        }
+        catch(e){
+            rejectPromise(e);
+        }
+    });
 }
 
 function _backup(file){
-
+    return Q.promise(function(resolvePromise, rejectPromise){
+        try{
+            b2.uploadFile(b2.createClient("xx", "xx"), function(err, data){
+                if(err){
+                    rejectPromise(err);
+                }
+                else{
+                    resolvePromise(data);
+                }
+            });
+        }
+        catch(e){
+            rejectPromise(e);
+        }
+    });
 }
 
 function _setMetadata(file, metadata){
